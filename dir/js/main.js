fc129615c7e742d5c1425e5d00ebb03d5346a0dc
Mahbub <mahbubalam12890@gmail.com>
2026-02-26 12:36:09 +0600

    fix changes

--- dir/js/main.js
+++ dir/js/main.js
@@ -1,38 +1,4 @@
-<<<<<<< HEAD
 
-
-window.addEventListener("DOMContentLoaded", () => {
-
-    const app = TubesCursor(document.getElementById('canvas'), {
-        tubes: {
-            colors: ["#f967fb", "#53bc28", "#6958d5"],
-            lights: {
-                intensity: 200,
-                colors: ["#83f36e", "#fe8a2e", "#ff008a", "#60aed5"]
-            }
-        }
-    })
-
-});
-
-
-
-
-document.body.addEventListener('click', () => {
-    const colors = randomColors(3)
-    const lightsColors = randomColors(4)
-    console.log(colors, lightsColors)
-    app.tubes.setColors(colors)
-    app.tubes.setLightsColors(lightsColors)
-})
-
-function randomColors(count) {
-    return new Array(count)
-        .fill(0)
-        .map(() => "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0'))
-}
-console.log(typeof TubesCursor);
-=======
 const starContainer = document.querySelector('.star-container');
 const aura = document.querySelector('.aura');
 
@@ -126,4 +92,4 @@ function animateAura(){
   requestAnimationFrame(animateAura);
 }
 animateAura();
->>>>>>> 4585f75 (falling stars background animation)
+
